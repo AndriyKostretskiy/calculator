f5254a01af00daa5b79d61c0e51239e048a509e5
changecode <33571598+AndriyKostretskiy@users.noreply.github.com>
2018-10-08 10:52:18 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,40 +1,41 @@
 const display = document.querySelector('.display');
+let result = display.value;
 let memory;
 
 document.querySelectorAll('.digits, .operators')
-    .forEach(el => el.addEventListener('click', e => display.value += e.target.innerText));
+    .forEach(el => el.addEventListener('click', e => result += e.target.innerText));
     
 document.querySelector('.equal')
-        .addEventListener('click', () => (eval(display.value) === Infinity || eval(display.value) === -Infinity) ?
-                                          display.value = 'Dividing by zero is impossible!' :
-                                          display.value = eval(display.value));
+        .addEventListener('click', () => (eval(result) === Infinity || eval(result) === -Infinity) ?
+                                          result = 'Dividing by zero is impossible!' :
+                                          result = eval(result);
         
 document.querySelector('.delete-last-number')
-    .addEventListener('click', () => display.value = display.value.substring(0, display.value.length - 1)); 
+    .addEventListener('click', () => result = result.substring(0, result.length - 1)); 
     
 document.querySelector('.clear')
-    .addEventListener('click', () => display.value = '');       
+    .addEventListener('click', () => result = '');       
     
 document.querySelector('.degree-two')
-    .addEventListener('click', () => display.value = Math.pow(display.value, 2)); 
+    .addEventListener('click', () => result = Math.pow(result, 2)); 
     
 document.querySelector('.degree-three')
-    .addEventListener('click', () => display.value = Math.pow(display.value, 3)); 
+    .addEventListener('click', () => result = Math.pow(result, 3)); 
     
 document.querySelector('.square')
-    .addEventListener('click', () => display.value = Math.sqrt(display.value));   
+    .addEventListener('click', () => result = Math.sqrt(result));   
     
 document.querySelector('.plus-minus')
-    .addEventListener('click', () => display.value = -display.value);    
+    .addEventListener('click', () => result = -result);    
     
 document.querySelector('.memory-read')
-    .addEventListener('click', () => display.value += memory); 
+    .addEventListener('click', () => result += memory); 
     
 document.querySelector('.memory-save')
-    .addEventListener('click', () => memory = display.value);
+    .addEventListener('click', () => memory = result);
     
 document.querySelector('.memory-clear')
     .addEventListener('click', () => memory = '');
     
 document.querySelector('.piConstant')
-    .addEventListener('click', () => memory = display.value += Math.PI.toFixed(2));    
+    .addEventListener('click', () => result += Math.PI.toFixed(2));    
